41b0dbc3122ad1738bd1aeedc5bfe7b17f060e23
Joe Grainger <joe@venncreative.co.uk>
2014-06-06 10:32:37 +0100

    remove wrapper function options_merge, close #13

--- src/CPT.php
+++ src/CPT.php
@@ -269,23 +269,6 @@ class CPT {
     }
 
 
-    /*
-        helper function options_merge
-        merges user submitted options array with default settings
-        preserving default data if not included in user options
-
-        @param  array  $defaults        the default option array
-        @param  array  $options         user submitted options to add/override defaults
-
-    */
-
-    function options_merge($defaults, $options) {
-
-        return array_replace_recursive($defaults, $options);
-
-    }
-
-
 
     /*
         helper function add_action
@@ -489,7 +472,7 @@ class CPT {
             );
 
         // merge user submitted options with defaults
-        $options = $this->options_merge($defaults, $this->options);
+        $options = array_replace_recursive($defaults, $this->options);
 
         // set the object options as full options passed
         $this->options = $options;
@@ -599,7 +582,7 @@ class CPT {
         );
 
         // merge default options with user submitted options
-        $options = $this->options_merge($defaults, $options);
+        $options = array_replace_recursive($defaults, $options);
 
         // add the taxonomy to the object array
         // this is used to add columns and filters to admin pannel
